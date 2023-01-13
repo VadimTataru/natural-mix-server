d5ba953292c1e1b47b1b3c570d1691d32ff05d5a
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2023-01-13 10:13:40 +0300

    test: add image recognize tests

--- NaturalMixApi.Tests/PostRequestParameterTests.cs
+++ NaturalMixApi.Tests/PostRequestParameterTests.cs
@@ -51,5 +51,42 @@ namespace NaturalMixApi.Tests
                 Console.WriteLine(ex.Message);
             }
         }
+
+        [Test]
+        [TestCase(2)]
+        [TestCase(2.4)]
+        [TestCase('a')]
+        [TestCase(true)]
+        public async Task Test_NaturalMixApi_RecognizeImage_False(object postBody)
+        {
+            var client = new HttpClient();
+            string url = "https://localhost:7246/api/ComponentItem/recognize";
+            try
+            {
+                using var response = await client.PostAsJsonAsync(url, postBody);
+                Assert.AreEqual(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        [Test]
+        public async Task Test_NaturalMixApi_RecognizeImage_True()
+        {
+            var client = new HttpClient();
+            string url = "https://localhost:7246/api/ComponentItem/recognize";
+            var postBody = new byte[1];
+            try
+            {
+                using var response = await client.PostAsJsonAsync(url, postBody);
+                Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
